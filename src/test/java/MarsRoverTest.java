e9caac6d7ca5cc498c4aef71c3b8aa1f432c21fb
Cédric Carré <cedric.carre@arolla.fr>
2022-11-22 19:20:12 +0100

    test: refactor and introduce parameterized test

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,19 +1,26 @@
 import fr.chaplinb.MarsRover;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MarsRoverTest {
-    @Test
-    void should_move_forward() {
+
+
+    @ParameterizedTest(name = "to position {1} when it receive commands {0} ")
+    @CsvSource({
+            "M,0 1 N"
+    })
+    void given_starting_of_0_0_and_heading_North_should_move_forward(String commands, String finalPosition) {
         //Arrange
         int x = 0;
         int y = 0;
         String orientation = "N";
         MarsRover rover = new MarsRover(x, y, orientation);
         //Act
-        rover.executeCommands("M");
+        rover.executeCommands(commands);
         //Assert
-        assertThat(rover.getPosition()).isEqualTo("0 1 N");
+        assertThat(rover.getPosition()).isEqualTo(finalPosition);
     }
 }
