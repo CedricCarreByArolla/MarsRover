2bdf596bfc754ebb1c30c7138d192fe573f4e8bc
Cédric Carré <cedric.carre@arolla.fr>
2022-11-22 18:46:23 +0100

    set environment up

--- src/test/java/MarsRover.java
+++ src/test/java/MarsRover.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class MarsRover {
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MarsRover {
+    @Test
+    void should_be_true() {
+        assertThat(true).isTrue();
+    }
 }
