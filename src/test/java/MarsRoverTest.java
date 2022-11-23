61ba4c1738a3cec2ab711703e2dc6380c329ef8e
Cédric Carré <cedric.carre@arolla.fr>
2022-11-23 16:37:14 +0100

    test: add test to move forward when Est facing rover

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -22,4 +22,19 @@ class MarsRoverTest {
         //Assert
         assertThat(northFacingRover.getPosition()).isEqualTo(finalPosition);
     }
+
+    @ParameterizedTest(name = "to position {1} when it receive commands {0} ")
+    @CsvSource({
+            "M,1 0 E",
+    })
+    void given_starting_of_0_0_and_heading_Est_should_move_forward(String commands, String finalPosition) {
+        //Arrange
+        int x = 0;
+        int y = 0;
+        MarsRover estFacingRover = new MarsRover(x, y, "E");
+        //Act
+        estFacingRover.executeCommands(commands);
+        //Assert
+        assertThat(estFacingRover.getPosition()).isEqualTo(finalPosition);
+    }
 }
