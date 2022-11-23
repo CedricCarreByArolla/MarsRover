268e6b5aa43f329465e4c37301f9e6d313ec0072
Cédric Carré <cedric.carre@arolla.fr>
2022-11-23 17:30:04 +0100

    test: check the turn right command

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -76,4 +76,19 @@ class MarsRoverTest {
         //Assert
         assertThat(estFacingRover.getPosition()).isEqualTo(finalPosition);
     }
+
+    @ParameterizedTest(name = "to position {1} when it receive commands {0} ")
+    @CsvSource({
+            "R,0 0 E",
+    })
+    void given_starting_of_0_0_and_heading_North_should_turn_right(String commands, String finalPosition) {
+        //Arrange
+        int x = 0;
+        int y = 0;
+        MarsRover estFacingRover = new MarsRover(x, y, 'N');
+        //Act
+        estFacingRover.executeCommands(commands);
+        //Assert
+        assertThat(estFacingRover.getPosition()).isEqualTo(finalPosition);
+    }
 }
